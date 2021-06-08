cd783f216c47fa91aa118d4f5cfa6711c16be665
anita.tsai <anitatsai@anitatsaideMacBook-Pro.local>
2021-06-09 06:38:15 +0800

    add to 323

---
+++ udemy_lecture/323_Deploying Our Files/note1.js
@@ -0,0 +1,23 @@
+// how to deploy our backend code and database code
+// -> use 'Hiroku' to have another server
+
+// 1) a server is a computer
+// And we put our node files on this computer
+// We run the node file 
+// and the server is going to be running the entire time, 
+// just waiting, waiting to hear from the outside world 
+// until somebody pings us, makes an HTTP request 
+// and we do something with it.
+// -> So we're going to deploy our note code to Heroku.
+
+
+// 2) Hiroku
+// And Hiroku is going to give us 
+// for free a computer somewhere in the world 
+// that we can put our node.js files to our backend code.
+// And it's going to wait for us to be able to ping it whenever we want.
+// Now we could leave our front, 
+// encode our Smart Brain app on GitHub pages.
+
+// 3) show you how to create a server
+// not just in your own computer
