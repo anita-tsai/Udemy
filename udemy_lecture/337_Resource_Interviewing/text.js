d117ef925c16367bc7bbf56b6a23ed1c95e48c03
anita-tsai <anita83110@gmail.com>
2021-06-25 01:15:43 +0800

    add to 337

---
+++ udemy_lecture/337_Resource_Interviewing/text.js
@@ -0,0 +1,21 @@
+// Resource: Interviewing
+
+/*
+
+I actually recently released a course 
+to help you through the interview process 
+and land a dream job as a developer. 
+You can see the course and the discount codes 
+in the Extras section (very last one) titled: 
+Part 2 - Special Gift For You
+
+These are the best resources I have found on interviewing
+and landing a job/career as a developer. 
+I hope you find them useful:
+
+1. https://haseebq.com/how-to-break-into-tech-job-hunting-and-interviews/
+2. http://seldo.com/weblog/2014/08/26/you_suck_at_technical_interviews
+3. Offer Negotiation
+4. https://medium.freecodecamp.org/5-key-learnings-from-the-post-bootcamp-job-search-9a07468d2331
+5. https://medium.com/on-writing-code/how-to-win-the-coding-interview-71ae7102d685
+*/
