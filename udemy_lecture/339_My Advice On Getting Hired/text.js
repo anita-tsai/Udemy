8fae4f39a1433277628d7be1809bcb2db47d93de
anita-tsai <anita83110@gmail.com>
2021-06-25 01:34:42 +0800

    add to 339

---
+++ udemy_lecture/339_My Advice On Getting Hired/text.js
@@ -0,0 +1,13 @@
+// My Advice On Getting Hired
+
+/*
+If you never ask, the answer is always no.
+
+You should value yourself. 
+Most importantly, you should value the fact 
+that you are giving your most precious resource (time) to an employer. 
+Make the right choice when selecting employers. 
+It is as much about what you want as it is about what they want. 
+Work for a company that respects your time. 
+Otherwise why would you want to be there?
+*/
