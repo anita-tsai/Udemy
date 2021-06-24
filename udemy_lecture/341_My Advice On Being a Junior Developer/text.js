ec6dee48380ab65487018b2c918715f0ccd44eb3
anita-tsai <anita83110@gmail.com>
2021-06-25 01:54:43 +0800

    add to 341

---
+++ udemy_lecture/341_My Advice On Being a Junior Developer/text.js
@@ -0,0 +1,87 @@
+// My Advice On Being a Junior Developer
+
+/**
+
+This is from my article 
+(originally posted on medium, now on my blog!). 
+You can read the full version at this link:
+https://zerotomastery.io/blog/dont-be-a-junior-developer
+
+*Don't be a Junior Developer
+
+Seriously, don’t be a junior developer. 
+A junior developer puts this title in their resume, emails, and LinkedIn… 
+They pronounce it to the world. Don’t.
+
+When you do that, this is what recruiters and companies see: 
+“Hi, I’m desperately looking to get hired as a developer. 
+I’m still new at this, but can you please please please place a bet on me 
+and hope that I turn out to be an asset and not a liability for your company. 
+Oh, and I’m also going to need a lot of help from your staff for the first 6 months!”
+
+1. But, I AM a junior developer!… you say. 
+If that is the case, then you will have better long term success 
+if you focus on improving your skills to become an intermediate developer. 
+Only then, you should start applying to jobs.
+Dedicate yourself full time on learning proper skills. 
+This way, you don’t pigeonhole yourself 
+to the “junior” developer role that you brand yourself as.
+
+Remember, first impressions are important. 
+By getting hired as a junior developer, 
+you will have to spend a longer time getting out of that role 
+than if you would have, 
+if you spent a little more time getting comfortable calling yourself an intermediate developer 
+and getting hired into that role right away.
+
+2. But when would I know when I’m not a junior developer?…you say. 
+You won’t. You will always feel like you don’t know enough. 
+You will always feel like others are smarter than you. 
+This is called imposter’s syndrome. 
+It’s normal and every developer feels it. 
+But here is a simple test for web developers: 
+Can you explain to your family members how the internet works? 
+How a computer works? 
+How websites work? 
+Do you have a basic understanding of HTML, CSS and Javascript so you can build your own websites?
+Do you know a little bit of React? 
+Have you built a few projects on your own on Github and you are comfortable putting up websites and apps online? 
+Good, then you are not a junior developer.
+
+3. But I need a job right now!..you say. 
+Stop that short term thinking. 
+Unless your job involves you working with really smart people 
+that you can learn from every day, 
+on technologies that are relevant and current 
+(few junior developer roles offer you this), 
+your time would be better invested learning skills 
+to get out of the junior mindset. 
+Long term, you will earn more money, 
+be with better developer teams, 
+and you will be more likely to work for a company 
+that teaches and let’s you work with up to date technologies every day. 
+Don’t work on updating a Wordpress plugin 
+as the resident junior developer of a law firm. 
+That won’t help you long term.
+
+If you apply for junior developer roles, 
+the best case scenario: You become a junior developer.
+
+If you apply for intermediate developer roles, 
+the best case scenario: You become an intermediate developer.
+
+Don’t sell yourself short.
+
+
+
+If you take away one thing from this article…
+Stop calling yourself a junior developer. 
+Have a junior developer mindset 
+where you are constantly looking to learn from others, 
+but never settle for a junior developer role. 
+Apply for roles for which you are under qualified not overqualified. 
+Remember that if you never ask the answer will always be no.
+
+Don’t overestimate the world and underestimate yourself. 
+You are better than you think.
+*/
