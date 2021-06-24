513efe5e4e50deb77d6ad4d817409aded4fac321
anita-tsai <anita83110@gmail.com>
2021-06-25 02:02:14 +0800

    add to 343

---
+++ udemy_lecture/343_Thank you/text.js
@@ -0,0 +1,25 @@
+// Thank you
+
+/*
+
+Truly, thank you for completing the course. 
+I hope you are able to do what you want with this knowledge now. 
+I would love to hear from you about your experiences 
+and where this course has gotten you in your life.  
+You can also follow me on twitter where I will announce when my next course is out 
+(I am currently working on it!): @andreineagoie
+
+It will really mean a lot to me if you are able to leave a positive review 
+if you have enjoyed this course. 
+Reviews mean everything on a website like udemy, 
+and it is the only way I can be discovered by others. 
+
+Please keep in touch and share this course to anybody 
+that may benefit from it. 
+Until next time. Buh bye. 
+
+OH BY THE WAY, I have now released a part 2 of this course 
+that takes you on your next steps into more advanced topics: 
+
+The Complete Junior to Senior Web Developer Roadmap
+*/
