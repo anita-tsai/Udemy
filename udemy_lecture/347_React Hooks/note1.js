5c454f3a1c2d524ec545079f90aeab90c6a47915
anita-tsai <anita83110@gmail.com>
2021-06-25 18:32:39 +0800

    add to 348

---
+++ udemy_lecture/347_React Hooks/note1.js
@@ -0,0 +1,10 @@
+// if you have an application that is running, 
+// that has users, most of the time 
+// you want to keep things up to date.
+
+// React had a big update as a version sixteen point eight.
+// It got something called hooks.
+// Because it's a library, 
+// it gets updates because it's being used by companies like Facebook
+// that has teams that constantly need new things, 
+// find challenges and want to improve it.
---
+++ udemy_lecture/348_React Hooks 2/note1.js
@@ -0,0 +1,5 @@
+// https://zh-hant.reactjs.org/docs/hooks-overview.html
+
+// the first part of reading the documents 
+// is to see the motivation
+// you could understand why it is useful
