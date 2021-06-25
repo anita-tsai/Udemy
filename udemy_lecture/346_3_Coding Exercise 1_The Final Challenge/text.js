2c6628d735c3763a228041b285dd10ad33d4838b
anita-tsai <anita83110@gmail.com>
2021-06-25 16:03:30 +0800

    add to 347

---
+++ udemy_lecture/346_3_Coding Exercise 1_The Final Challenge/text.js
@@ -0,0 +1,17 @@
+//console.log 'goodbye'
+
+// 1)
+function goodBye() {
+    console.log('goodbye');
+}
+
+goodBye();
+
+
+
+// 2)
+const goodBye = () => {
+    console.log('goodbye');
+}
+
+goodBye();
