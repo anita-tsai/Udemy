24a9767b61b048b749d0db5a542a6ec870b22530
anita-tsai <anita83110@gmail.com>
2021-06-25 15:55:46 +0800

    add to 346

---
+++ udemy_lecture/346_Coding Challenges/text.js
@@ -0,0 +1,19 @@
+/*
+Coding Challenges
+
+We run monthly coding challenges as part of this course. 
+Each month the challenge is selected to improve a specific part 
+of what it takes to be a great developer. 
+These challenges will sometimes come with awesome prizes as well! 
+If you are looking for extra practice, project ideas, 
+or want to keep your skills up to date, 
+I recommend you keep an eye out on the announcement email from Udemy, 
+or follow the #coding-challenge channel on Discord. 
+
+I also recommend you try some of the past challenges 
+while comparing your code to other students from this course. 
+You can find all of the challenges that have happened in the past, 
+including the current one you can join here.
+https://zerotomastery.io/community/?utm_source=udemy&utm_medium=coursecontent
+https://zerotomastery.io/community/?utm_source=udemy&utm_medium=coursecontent
+*/
