6601caa349984a74bf2480f905e66bd020a5fa31
anita-tsai <anita83110@gmail.com>
2021-06-26 01:53:45 +0800

    add to 349

---
+++ udemy_lecture/349_React Hooks 3/note1.js
@@ -0,0 +1,17 @@
+// To convert our robofriends app from using classes
+// to using hooks
+// fork the previous project called robofriends
+
+
+/*  in terminal  */
+// 1) fork the project
+// git clone https://github.com/anita-tsai/my-robofriends.git
+// npm install
+// npm update
+// sudo npm install -g npm
+
+
+// 2) check the vesion of React(greater than 16.8)
+
+// 3) make sure that everything is still working
+// npm start
