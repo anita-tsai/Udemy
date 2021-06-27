d54ecc0d3150837a756f75e565f3246a096762db
anita-tsai <anita83110@gmail.com>
2021-06-28 01:15:07 +0800

    add to 65 Bootstrap

---
+++ udemy_lecture/65_Bootstrap Introduction/note1.js
@@ -0,0 +1,23 @@
+// resource:
+// https://getbootstrap.com/
+
+/*
+
+Bootstrap is one of the most popular projects ever for developers.
+
+And what it is, is just a css file and JavaScript file.
+
+But why would that be one of the most popular projects?
+Well, one thing you might have noticed 
+is that writing css from scratch is a bit of a pain.
+
+Wouldn't it be nice if you can use something 
+that has already been created by somebody so that not every
+single developer has to keep reinventing the wheel 
+and keep writing everything from scratch?
+
+And that is the idea behind bootstrap.
+Bootstrap was created to solve this problem 
+so that we can create websites really, really fast by using
+somebody else's css and building on top of it.
+*/
