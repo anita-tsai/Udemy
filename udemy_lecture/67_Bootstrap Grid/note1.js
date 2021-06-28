d35c7c9c592daea372f0b9d9e5d9efcdc0a7cfeb
anita-tsai <anita83110@gmail.com>
2021-06-28 23:42:10 +0800

    change the file 67

--- udemy_lecture/67_Bootstrap Grid/note1.js
+++ udemy_lecture/67_Bootstrap Grid/note1.js
@@ -2,3 +2,33 @@
 // 1. create a index.html file
 // 2. download the Bootstrap
 // 3. copy and paste the CDN link 
+// 4. create a style.css file
+
+
+/*  Bootstrap Grid  */
+//  you can create really responsive websites 
+//  and that was the biggest selling feature of Bootstrap, 
+//  it was solving this really really hard problem of 
+//  making sure that our website looked good no matter 
+//  what the screen size
+
+
+// 1) container
+// 1. go to: Grid -> Equal Width (copy those code)
+<div class="container">
+  <div class="row">
+    <div class="col">
+      1 of 2
+    </div>
+    <div class="col">
+      2 of 2
+    </div>
+  </div>
+</div>
+// 2. go to style.css
+  // .col {
+  //   background-color: grey;
+  //   border: 2px solid black;
+  // }
+
+// 3. adjust the colum size
