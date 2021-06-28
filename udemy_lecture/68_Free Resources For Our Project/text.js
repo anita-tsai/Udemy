2f631bd84509b0841e160d923d546cfbafee41c2
anita-tsai <anita83110@gmail.com>
2021-06-28 23:45:21 +0800

    add to 68

---
+++ udemy_lecture/68_Free Resources For Our Project/text.js
@@ -0,0 +1,12 @@
+// Free Resources For Our Project
+
+/*
+
+In the next couple of videos we will be building our own Startup Landing Page! 
+Although you can follow along and copy what I do, 
+you will also have the ability to create your own versions! 
+I have gathered all the free resources that you can use 
+to build your own website here: 
+https://zerotomastery.io/resources
+
+ */
