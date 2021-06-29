eca1841a20400f81470772273586c0c5b5288de5
anita-tsai <anita83110@gmail.com>
2021-06-29 17:53:29 +0800

    add to 72

---
+++ udemy_lecture/72_Exercise_Startup Landing Page 4/note1.js
@@ -0,0 +1,34 @@
+// 1) in index.html
+// 1. to capital words
+// use Bootstrap: add a class
+// -> 
+// <h1 class="text-uppercase">
+
+
+// 2. button 
+// use Bootstrap: Buttons
+// ->
+// <button class="btn btn-primary">
+
+// 2) button style
+// 1. add btn-xl in button tag 
+// 2. go to css to style btn-xl tag
+  // padding
+// 3. add btn tag in css and style it
+  // font-size
+  // border-radius
+  // text-transform
+// 4. add btn-primary tag in css and style it
+  // background-color
+  // border-color
+// 5. btn-primary:hover (to make it more black and become bigger)
+  // background-color
+  // border-color
+  // border-width
+
+// 3) add a horizontal line
+// 1. in index.html: <hr>
+// 2. in css: 
+  // border
+  // max-width
+  // opacity
