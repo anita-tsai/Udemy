12020e3f8801d00cd3acf32f5c2b0dc6e17b0af2
anita-tsai <anita83110@gmail.com>
2021-06-29 16:22:58 +0800

    add to 71

---
+++ udemy_lecture/71_Exercise_Startup Landing Page 3/note1.js
@@ -0,0 +1,25 @@
+// 
+// 1) in body
+// 1. add h1 tag
+// 2. add button
+
+// 2) in head
+// 1. add meta tags
+  /** Responsive meta tag
+  Bootstrap is developed mobile first, a strategy in which 
+  we optimize code for mobile devices first 
+  and then scale up components as necessary using CSS media queries. 
+  To ensure proper rendering and touch zooming for all devices, 
+  add the responsive viewport meta tag to your <head>.
+   */
+
+// 3) sereach full page background image
+// https://css-tricks.com/perfect-full-page-background-image/
+// add to our style.css
+
+// 4) in body
+// 1. add <strong>
+
+// 5)in css
+// 1. add a new h1 element
+// 2. add font-size and color in h1
---
+++ udemy_lecture/71_Exercise_Startup Landing Page 3/resource.js
@@ -0,0 +1,11 @@
+// 1. what is meta tag?
+// https://www.w3schools.com/tags/tag_meta.asp
+
+// 2. viewport meta tag
+// https://developer.mozilla.org/en-US/docs/Mozilla/Mobile/Viewport_meta_tag
+
+// 3. what is UTF-8?
+// https://www.quora.com/What-is-UTF8
+
+// 4. CSS Tricks Perfect Background
+// https://css-tricks.com/perfect-full-page-background-image/
