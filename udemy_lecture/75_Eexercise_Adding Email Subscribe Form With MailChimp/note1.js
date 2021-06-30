5b6abb226ce6b0491d816e25cc090bfdcd241359
anita-tsai <anita83110@gmail.com>
2021-06-30 22:30:03 +0800

    add to 75

---
+++ udemy_lecture/75_Eexercise_Adding Email Subscribe Form With MailChimp/note1.js
@@ -0,0 +1,14 @@
+// resource:
+// 1. MailChimp
+// https://mailchimp.com/
+// 2. How to Create a Landing Page
+// https://mailchimp.com/help/create-a-landing-page/
+
+
+// goal: 
+// To make people have a way to submit their emails
+
+// 1) MailChimp
+// it's a email marketing service
+// 1. go to:
+// audience -> manage audience-> signup forms
