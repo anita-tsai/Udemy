de78f7b951f8b0e0943f77691d70839387a246e8
anita-tsai <anita83110@gmail.com>
2021-06-30 22:33:14 +0800

    add to 76

---
+++ udemy_lecture/76_Quick Note_Upcoming Videos/text.js
@@ -0,0 +1,16 @@
+// 76. Quick Note: Upcoming Videos
+
+/**
+
+In the next 2 videos,
+we show you how to put your startup landing page online for free! 
+I recommend you watch the first video 
+Exercise: Putting Your Website Online 
+without following along yourself. 
+This is because there is now an easier way to put a website online
+that I will show with the updated Exercise: Putting Your Website Online 2. 
+However, I kept the original video in here because 
+it is still a valid and useful method! 
+Enjoy them both but I recommend only following along with the second method!
+
+ */
