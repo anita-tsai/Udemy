d68287b53df53a1d72b317fe3ae0589ff5554a90
anita-tsai <anita83110@gmail.com>
2021-07-01 16:01:18 +0800

    add to 351

---
+++ udemy_lecture/351_React Hooks 5/note1.js
@@ -0,0 +1,7 @@
+// goal:
+// use hook to convert our project with 'useState', 'useEffect'
+
+// 1) import 'useState' and 'useEffect' to 'react' and delete 'Component'
+
+// delete render()
+// delete 'this'
