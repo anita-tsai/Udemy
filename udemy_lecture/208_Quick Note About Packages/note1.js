ce2f4c246f25266467995f8705028bfe7a46c1af
anita-tsai <anita83110@gmail.com>
2021-07-03 01:10:15 +0800

    add 208

---
+++ udemy_lecture/208_Quick Note About Packages/note1.js
@@ -0,0 +1,9 @@
+// 208. Quick Note About Packages
+
+/**
+
+We will revisit this idea of package.json 
+and how to keep packages up to date at the end of our first React project, 
+in the lesson titled: Keeping Your Projects Up To Date! 
+
+ */
