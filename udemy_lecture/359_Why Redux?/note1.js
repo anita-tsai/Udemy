a97eee10a8eefb1ce85b177c54d4503388a7ff36
anita-tsai <anita83110@gmail.com>
2021-07-06 23:51:59 +0800

    add to 359

---
+++ udemy_lecture/359_Why Redux?/note1.js
@@ -0,0 +1,14 @@
+// 359. Why Redux?
+
+/*
+1. Good for managing large state
+
+2. Useful for sharing data between containers
+
+3. Predictable state management using the 3 priciples
+*/
+
+// The 3 priciples
+// 1. Single source of truth
+// 2. State is read only
+// 3. Changes using pure funtions
