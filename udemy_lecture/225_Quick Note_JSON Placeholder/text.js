e7d78c77bb0704cce9f591c8439ecfad86f24441
anita-tsai <anita83110@gmail.com>
2021-07-06 23:18:41 +0800

    add 225

---
+++ udemy_lecture/225_Quick Note_JSON Placeholder/text.js
@@ -0,0 +1,10 @@
+// 225. Quick Note: JSON Placeholder
+
+// In the next video, I will be showing you how to use this: 
+// https://jsonplaceholder.typicode.com/
+
+// Sometimes, depending on incidents, the website may be down. 
+// In that case, you can use an alternative here: 
+// https://jsonplaceholder.cypress.io/
+
+// You will see how to use it in the next video!
